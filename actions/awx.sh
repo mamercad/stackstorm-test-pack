b49cf1e3e1296e6002b3f113b8aed62a6405c966
Mark Mercado <mamercad@gmail.com>
2022-05-13 20:24:56 -0400

    Adding an awx action

---
+++ actions/awx.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TOWER_HOST="$1"
+TOWER_OAUTH_TOKEN="$2"
+JOB_TEMPLATE="$3"
+
+awx --conf.host "$TOWER_HOST" --conf.token "$TOWER_OAUTH_TOKEN" --conf.color "f" \
+  job_template launch --wait "${JOB_TEMPLATE}" -f "json"

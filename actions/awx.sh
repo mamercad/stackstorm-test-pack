860e6bd97e6cbe7641398496f720a4b459103c45
Mark Mercado <mamercad@gmail.com>
2022-05-13 21:13:11 -0400

    Adding job host summary parsing

--- actions/awx.sh
+++ actions/awx.sh
@@ -7,8 +7,18 @@ EXTRA_VARS="$4"
 INVENTORY="$5"
 LIMIT="$6"
 
+TMP_FILE="/tmp/${0}.$$"
+
 source "/opt/stackstorm/virtualenvs/${ST2_ACTION_PACK_NAME}/bin/activate"
 
 awx --conf.host "$TOWER_HOST" --conf.token "$TOWER_OAUTH_TOKEN" \
-    job_template launch --wait --inventory "$INVENTORY" --limit "$LIMIT" --extra_vars "$EXTRA_VARS" \
-      "${JOB_TEMPLATE}"
+    job_template launch --wait --inventory "$INVENTORY" --limit "$LIMIT" \
+      --extra_vars "$EXTRA_VARS" \
+        "${JOB_TEMPLATE}" \
+          | tee "${TMP_FILE}"
+
+JOB_ID="$(cat $TMP_FILE | jq -r .job)"
+
+curl -s -H "Authorization: Bearer ${TOWER_OAUTH_TOKEN}" \
+  "${TOWER_HOST}/api/v2/jobs/${JOB_ID}/job_host_summaries/" \
+    | jq -r '.results[] | .host_name, .failed'

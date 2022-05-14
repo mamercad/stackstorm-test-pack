63d22c0f6056af9b23130a55ff9665d33ebdca3f
Mark Mercado <mamercad@gmail.com>
2022-05-14 16:13:55 -0400

    Iterating on test

--- actions/test.py
+++ actions/test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 from st2common.runners.base_action import Action
-from pprint import pprint
+import os
+
 
 class TestAction(Action):
     def run(self, *args, **kwargs):
-        pprint(args)
-        pprint(kwargs)
+        print(os.environ)

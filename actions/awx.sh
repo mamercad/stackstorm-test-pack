ecff782602c215da99f90a171b2084917868438c
Mark Mercado <mamercad@gmail.com>
2022-05-13 20:39:13 -0400

    Source the virtualenv

--- actions/awx.sh
+++ actions/awx.sh
@@ -4,5 +4,7 @@ TOWER_HOST="$1"
 TOWER_OAUTH_TOKEN="$2"
 JOB_TEMPLATE="$3"
 
+source "/opt/stackstorm/virtualenvs/${ST2_ACTION_PACK_NAME}/bin/activate"
+
 awx --conf.host "$TOWER_HOST" --conf.token "$TOWER_OAUTH_TOKEN" --conf.color "f" \
   job_template launch --wait "${JOB_TEMPLATE}" -f "json"

2e19bdce908584e977a00fbbc37d095409a8e716
Mark Mercado <mamercad@gmail.com>
2022-05-13 20:54:43 -0400

    Adding extra vars

--- actions/awx.sh
+++ actions/awx.sh
@@ -3,11 +3,12 @@
 TOWER_HOST="$1"
 TOWER_OAUTH_TOKEN="$2"
 JOB_TEMPLATE="$3"
-INVENTORY="$4"
-LIMIT="$5"
+EXTRA_VARS="$4"
+INVENTORY="$5"
+LIMIT="$6"
 
 source "/opt/stackstorm/virtualenvs/${ST2_ACTION_PACK_NAME}/bin/activate"
 
 awx --conf.host "$TOWER_HOST" --conf.token "$TOWER_OAUTH_TOKEN" --conf.color "f" -f "json" --wait \
-    job_template launch --inventory "$INVENTORY" --limit "$LIMIT" \
+    job_template launch --inventory "$INVENTORY" --limit "$LIMIT" --extra_vars "$EXTRA_VARS" \
       "${JOB_TEMPLATE}"

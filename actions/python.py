64a814d2b5c805a7fd0db1fddf91c134657e38fe
Mark Mercado <mamercad@gmail.com>
2022-12-14 07:17:16 -0500

    Iterating

--- actions/python.py
+++ actions/python.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from st2common.runners.base_action import Action
+from lib.statsd import StatsDClient
 
 
 class PythonAction(Action):
     def run(self, *args, **kwargs):
         print("Python works!")
+        statsd = StatsDClient()
+        statsd.emit()
         return True, {"foo": "bar"}

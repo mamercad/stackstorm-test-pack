1c883ac0cd3563e0017c82928ba555cfc8de4e22
Mark Mercado <mamercad@gmail.com>
2022-12-14 06:01:39 -0500

    Iterating

--- actions/python.py
+++ actions/python.py
@@ -6,4 +6,4 @@ from st2common.runners.base_action import Action
 class PythonAction(Action):
     def run(self, *args, **kwargs):
         print("Python works!")
-        return (True, {"foo": "bar"})
+        return True, {"foo": "bar"}

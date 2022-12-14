a4e8c01e3a987e74bf08bd66b1bee8673956cd06
Mark Mercado <mamercad@gmail.com>
2022-12-14 17:26:39 -0500

    Add StatsD Counter Action

---
+++ actions/statsd_counter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import time
+from st2common.runners.base_action import Action
+from statsd_client import StatsDClient
+
+
+class StatsDCounterAction(Action):
+    def run(self, *args, **kwargs):
+        name = args.get("name")
+
+        statsd = StatsDClient()
+        statsd.incr(counter=name)
+
+        return True

d5fbcdd0f60d3e174cd15843b95d8f39e29f9cc3
Mark Mercado <mamercad@gmail.com>
2022-12-15 06:10:52 -0500

    Iterating

--- actions/kv_timing.py
+++ actions/kv_timing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# import time
+import time
 from st2common.runners.base_action import Action
 from st2client.client import Client
 from st2client.models import KeyValuePair
@@ -8,5 +8,16 @@ from st2client.models import KeyValuePair
 class StatsDCounterAction(Action):
     def run(self, *args, **kwargs):
         client = Client(base_url='http://localhost')
-        client.keys.update(KeyValuePair(name='foo', value='bar'))
-        return True
+
+        name = kwargs.get("name")
+        start = kwargs.get("start")
+        epoch_now = time.time()
+
+        if start:
+            client.keys.update(KeyValuePair(name=name, value=epoch_now))
+            return True, {"epoch_now": epoch_now}
+
+        epoch_start = client.keys.get_by_name(name=name).value
+        duration_seconds = epoch_now - epoch_start
+        client.keys.delete(epoch_start)
+        return True, {"duration_seconds": duration_seconds}

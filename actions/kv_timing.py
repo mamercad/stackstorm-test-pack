f418dd9a788ca4fc469ef00fcd0908842a0b141e
Mark Mercado <mamercad@gmail.com>
2022-12-20 18:21:15 -0500

    Make flake8 happy

--- actions/kv_timing.py
+++ actions/kv_timing.py
@@ -5,6 +5,7 @@ from st2common.runners.base_action import Action
 from st2client.client import Client
 from st2client.models import KeyValuePair
 
+
 class StatsDCounterAction(Action):
     def run(self, *args, **kwargs):
         client = Client(base_url='http://localhost')

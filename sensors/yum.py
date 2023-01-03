430420c26ae1c1c6d8639668aace9559831f0060
Mark Mercado <mamercad@gmail.com>
2023-01-03 13:39:57 -0500

    Iterating

--- sensors/yum.py
+++ sensors/yum.py
@@ -3,7 +3,7 @@ from st2reactor.sensor.base import PollingSensor
 
 
 class YumSensor(PollingSensor):
-    def __init__(self, sensor_service, config, poll_interval=20):
+    def __init__(self, sensor_service, config, poll_interval=60):
         super(YumSensor, self).__init__(
             sensor_service=sensor_service, config=config, poll_interval=poll_interval
     )
@@ -19,7 +19,7 @@ class YumSensor(PollingSensor):
             "host4": randint(0,10),
             "host5": randint(0,10),
         }
-        self._sensor_service.dispatch(trigger=self._trigger_ref, payload=payload)
+        self._sensor_service.dispatch(trigger="test.yum", payload=payload)
 
     def cleanup(self):
         # This is called when the st2 system goes down. You can perform cleanup operations like

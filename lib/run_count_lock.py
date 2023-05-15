6cfe750a53c9b023ee083d28764fb177bfc829e2
Mark Mercado <mamercad@gmail.com>
2023-05-15 07:58:59 -0400

    Testing

--- lib/run_count_lock.py
+++ lib/run_count_lock.py
@@ -25,7 +25,7 @@ class RunCountLock:
 
         self.lock_file = open(self.lock_path, "r+")
 
-    def count(self):
+    def count(self) -> int:
         self.lock_file.seek(0)
         return self.lock_file.readlines()[-1].strip()
 
